08a6fb229a15121e115de8e38bcc2a56a4a52479
kebabjohn <32761420+kebabjohn@users.noreply.github.com>
2018-01-10 13:35:28 +0100

    Initial commit

---
+++ app/src/main/java/com/example/patri/wakeupapp/WakeUp.java
@@ -0,0 +1,13 @@
+package com.example.patri.wakeupapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WakeUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wake_up);
+    }
+}
